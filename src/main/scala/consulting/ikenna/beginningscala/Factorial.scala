051eb2f702525729a3b9452a1138951fa4e99a6c
Olummy <olumideoyalola@gmail.com>
2017-03-14 18:26:12 +0100

    Fibonacci Done

--- src/main/scala/consulting/ikenna/beginningscala/Factorial.scala
+++ src/main/scala/consulting/ikenna/beginningscala/Factorial.scala
@@ -3,6 +3,12 @@ package consulting.ikenna.beginningscala
 
 
 object Factorial {
-  def nonTailRecursiveFactorial(n: Int): Int =  ???
-  def tailRecursiveFactorial(n: Int): Int =  ???
-}
+  def nonTailRecursiveFactorial(n: Int): Int =  if (n == 0 | n ==1) 1 else n * nonTailRecursiveFactorial(n-1)
+  def tailRecursiveFactorial(n: Int): Int =  {
+    def innerRecursive(n: Int, prev: Int, acc: Int): Int = {
+      if (n == 0) acc else innerRecursive(n-1, acc, acc + prev)
+          }
+       return innerRecursive(n, 1, 0)
+      }
+  }
+
